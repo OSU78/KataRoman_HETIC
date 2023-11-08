f96778f04e8fd2a0b51cd036df80bd9ddd5fd9a1
jordan kaya <jkaya@greenflex.com>
2023-11-08 12:00:52 +0100

    add function to convert arabic number to roman number

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //
+function convertArabicNumberToRomanNumber (numberToConvert) {
+    if (isNaN(numberToConvert)){
+        return "veuillez rentrer un nombre";
+    }
+    let numerical_symbol = String(+numberToConvert).split("");
+     let keytables = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"];
+        let romanNumber = "";
+        let i = 3;
+    for(let i =2;i>-1;i--){
+        romanNumber = (keytables[+numerical_symbol.pop() + (i * 10)] || "") + romanNumber;
+    }
+    return Array(+numerical_symbol.join("") + 1).join("M") + romanNumber;
+}
