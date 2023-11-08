f970515bf92c4a8fa1d1a2510a39140c34baa5d3
Ousmane Salamatao <ousmanesalamatao782@gmail.com>
2023-11-08 14:19:13 +0100

    update roman to number

--- dom_interact.js
+++ dom_interact.js
@@ -2,6 +2,13 @@
 document.querySelector("#update").addEventListener("click", convert);
 
 function convert(){
-    var result = document.getElementById("resultat").innerHTML = convertArabicNumberToRomanNumber(document.getElementById("number").value);
+    let number = document.getElementById("number").value
+    if(!isNaN(document.getElementById("number").value)){
+        let result = document.getElementById("resultat").innerHTML = convertArabicNumberToRomanNumber(number);
+   
+    }
+    else{
+        let result = document.getElementById("resultat").innerHTML = convertRomanNumberToArabicNumber(number);
+    }
     
 }
--- function.js
+++ function.js
@@ -1,7 +1,19 @@
 //
+
+function isValidRomanNumber(romanNumber) {
+    console.log(romanNumber);
+    // Expression régulière pour valider un nombre romain
+    const romanRegex = /^(?=[MDCLXVI])(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))$/i;
+
+    
+    return romanRegex.test(romanNumber);
+}
+
+
+
 function convertArabicNumberToRomanNumber (numberToConvert) {
     if (isNaN(numberToConvert)){
-        return "veuillez rentrer un nombre";
+        return "veuillez rentrer un nombre valide";
     }
     let numerical_symbol = String(+numberToConvert).split("");
      let keytables = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
@@ -14,5 +26,44 @@ function convertArabicNumberToRomanNumber (numberToConvert) {
     }
     return Array(+numerical_symbol.join("") + 1).join("M") + romanNumber;
 }
+
+function convertRomanNumberToArabicNumber(romanNumber) {
+    if (!isValidRomanNumber(romanNumber)){
+        return "veuillez rentrer un nombre valide";
+    }
+    // Map des valeurs des chiffres romains
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let arabicNumber = 0;
+    let previousValue = 0;
+
+    for (let i = romanNumber.length - 1; i >= 0; i--) { 
+        const currentValue = romanMap[romanNumber.charAt(i).toUpperCase()];
+
+        if (currentValue < previousValue) {
+            // Si la valeur actuelle est inférieure à la précédente
+            arabicNumber -= currentValue;
+        } else {
+            // Sinon, ajoutez-la
+            arabicNumber += currentValue;
+        }
+
+        previousValue = currentValue;
+    }
+
+    return arabicNumber;
+}
+
+
+
+
 
 
